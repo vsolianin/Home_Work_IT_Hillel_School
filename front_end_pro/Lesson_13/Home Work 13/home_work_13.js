44bb5680115f904ff58011c9ca36a415febf7689
Volodymyr <vsolianin@gmail.com>
2019-12-27 17:29:13 +0200

    v0.2

--- front_end_pro/Lesson_13/Home Work 13/home_work_13.js
+++ front_end_pro/Lesson_13/Home Work 13/home_work_13.js
@@ -1,10 +1,20 @@
 window.onload = function() {
 
-var first_input = document.querySelector("#first_input").value;
-var second_input = document.querySelector("#first_input").value;
-var third_input = document.querySelector("#first_input").value;
+function oninput() {
+	var text_area = document.querySelector("#text");
 
-
-var text_area = document.querySelector("#text");
-console.dir(text_area);
+	var first_input = document.querySelector("#first_input").value;
+	var second_input = document.querySelector("#first_input").value;
+	var third_input = document.querySelector("#first_input").value;
+	console.log(first_input.length);
+	(function() {
+		if(i < first_input.length) {
+			text_area.value += first_input[i];
+			i++;
+		}
+	}())
 }
+
+}
+
+oninput();
