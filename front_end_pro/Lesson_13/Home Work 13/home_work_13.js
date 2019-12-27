66e53baa8bd692011329eb23d0f7e8d3debce7f5
Volodymyr <vsolianin@gmail.com>
2019-12-27 15:19:57 +0200

    v0.1

---
+++ front_end_pro/Lesson_13/Home Work 13/home_work_13.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+var first_input = document.querySelector("#first_input").value;
+var second_input = document.querySelector("#first_input").value;
+var third_input = document.querySelector("#first_input").value;
+
+
+var text_area = document.querySelector("#text");
+console.dir(text_area);
+}
