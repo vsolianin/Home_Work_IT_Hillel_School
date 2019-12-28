7aed52cfc0ab179f9b4fe34477060bd3ffb82003
Volodymyr <vsolianin@gmail.com>
2019-12-28 03:37:50 +0200

    v0.3

--- front_end_pro/Lesson_13/Home Work 13/home_work_13.js
+++ front_end_pro/Lesson_13/Home Work 13/home_work_13.js
@@ -1,20 +1,63 @@
 window.onload = function() {
 
-function oninput() {
+	
 	var text_area = document.querySelector("#text");
+	console.dir(text_area);
+	var first_input = document.querySelector("#first_input");
+	var second_input = document.querySelector("#second_input");
+	var third_input = document.querySelector("#third_input");
+	var first = [];
+	var second = [];
+	var third = [];
+	var main = [];
+	console.log(main, "main");
+	console.log(first, "first");
+	first_input.oninput = function(event) {
 
-	var first_input = document.querySelector("#first_input").value;
-	var second_input = document.querySelector("#first_input").value;
-	var third_input = document.querySelector("#first_input").value;
-	console.log(first_input.length);
-	(function() {
-		if(i < first_input.length) {
-			text_area.value += first_input[i];
-			i++;
+		if(event.data == null) {
+			(function() {
+				first.concat(first.pop(event.data));
+				text_area.textContent = first.join("");
+			}());
+		} else {
+			(function() {
+				first.concat(first.push(event.data));
+				text_area.textContent = first.join("");
+			}());
 		}
-	}())
-}
+		console.log(first, "first");
+		console.log(text_area.textContent, "text_area.textContent");
+		// console.log(main, "main");
+	}
+
+	second_input.oninput = function(event) {
+
+		if(event.data == null) {
+			(function() {
+				second.concat(second.pop(event.data));
+				text_area.textContent = second.join("");
+			}());
+		} else {
+			(function() {
+				second.concat(second.push(event.data));
+				text_area.textContent = second.join("");
+			}());
+		}
+		
+	}
+
+	third_input.oninput = function(event) {
+
+		if(event.data == null) {
+			(function() {
+				text_area.textContent = text_area.textContent.slice(0, -1);
+			}());
+		} else {
+			(function() {
+				text_area.textContent += event.data;
+			}());
+		}
+		console.log(text_area.textContent, "third");
+	}
 
 }
-
-oninput();
