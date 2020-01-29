769c5e017fbe6a912eedc0fd3b9d31c28187f6ed
Volodymyr <vsolianin@gmail.com>
2020-01-30 01:50:12 +0200

    Final

--- front_end_pro/Lesson_14/home_work_14/home_work_14.js
+++ front_end_pro/Lesson_14/home_work_14/home_work_14.js
@@ -1,15 +1,14 @@
 window.onload = function() {
 
-	var box = document.querySelector('.box');
-	console.dir(box, 'box');
-	var click = document.querySelector('html');
-	console.dir(click, 'click');
+	var box = document.querySelector(".box");
+	var click = document.querySelector("html");
 
 	box.onclick = function() {
-		this.classList.toggle('colorBorder');	
+		this.classList.toggle("colorBorder");	
 	}
 
 	click.onclick = function(event) {
+		console.log(event, "html");
 
 		var scaleX = event.clientX;
 		var scaleY = event.clientY;
@@ -20,24 +19,47 @@ window.onload = function() {
 
 	box.onmousedown = function(event) {
 
-		moveAt(event.clientX, event.clientY);
+		var layerX = event.layerX;
+		var layerY = event.layerY;
 
-		function moveAt(clientX, clientY) {
+		function moveBox(pageX, pageY) {
 
-			box.style.left = clientX + "px";
-			box.style.top = clientY + "px";
+			box.style.left = pageX - layerX + "px";
+			box.style.top = pageY - layerY + "px";
 		}
 
+		moveBox(event.pageX, event.pageY);
+
 		function onMouseMove(event) {
-			moveAt(event.clientX, event.clientY);
+
+			moveBox(event.pageX, event.pageY);
 		}
 
 		document.addEventListener("mousemove", onMouseMove);
 
-		box.ommouseup = function(event) {
+		box.onmouseup = function(event) {	
+
 			document.removeEventListener("mousemove", onMouseMove);
+			moveBox(event.pageX, event.pageY);
+
 			box.onmouseup = null;
+			click.onclick = null;
+
+			setTimeout(function() {
+				click.onclick = function(event) {
+					console.log(event, "html");
+
+					var scaleX = event.clientX;
+					var scaleY = event.clientY;
+
+					box.style.left = scaleX + "px";
+					box.style.top = scaleY + "px";
+				}
+			}, .10)
 		};
 
+	};
+		box.ondragstart = function() {
+  			return false;
 	};
 }
